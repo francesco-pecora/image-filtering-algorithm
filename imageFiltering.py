116d6115bd1e9284fb2ac5ca6570585f25721a8b
Francesco Pecora <francescogp9898@hotmail.com>
2020-12-13 23:43:54 -0500

    improved median algorithm (order statistics + docs

--- imageFiltering.py
+++ imageFiltering.py
@@ -1,11 +1,10 @@
 """
 @class:  CSC382
 @author: Francesco Pecora
-@date:   12/07/2020
+@date:   12/13/2020
 """
 
 import cv2
-from numpy import median
 from medianDeterministic import DeterministicMedian
 
 class ImageFilterMedian:
@@ -15,6 +14,9 @@ class ImageFilterMedian:
         self.median = DeterministicMedian()
     
     def filterImage(self):
+        '''
+        main function where the filtering of the image starts
+        '''
         rows = len(self.img)
         cols = len(self.img[0])
         for row in range(rows):
@@ -23,15 +25,31 @@ class ImageFilterMedian:
         return self.img
     
     def findMedianOfNeighbours(self, row, col):
+        '''
+        function that calls the class finding the median of the window section (neighbours)
+        int row: vertical position of the cell to be processed
+        int col: horizontal position of the cell to be processed
+        '''
         neighbours = self.getNeighbours(row, col)
-        return self.median.quickselect_median(neighbours)
+        neighbours = list(set(neighbours)) # the order statistics algorithm assumes there are no repeating values
+        return self.median.kthSmallest(neighbours, 0, len(neighbours) - 1, len(neighbours)//2)
 
     def getNeighbours(self, row, col):
+        '''
+        function that identifies the subwindow around the specific cell of the matrix
+        int row: vertical position of the cell to be processed
+        int col: horizontal position of the cell to be processed
+        '''
         topLeft = (row - self.windowSize, col - self.windowSize)
         bottomRight = (row + self.windowSize, col + self.windowSize)
         return self.traverseSubWindow(topLeft, bottomRight)
 
     def traverseSubWindow(self, topLeft, bottomRight):
+        '''
+        function that flattens the subWindow into a 1-D array
+        int topLeft: integer holding the top left point(x, y) of the subWindow
+        int bottomRight: integer holding the bottom left point(x, y) of the subWindow
+        '''
         neighbours = []
         for row in range(topLeft[0], bottomRight[0] + 1):
             for col in range(topLeft[1], bottomRight[1] + 1):
@@ -40,6 +58,11 @@ class ImageFilterMedian:
         return neighbours
     
     def isInBounds(self, row, col):
+        '''
+        function that returns whether or not the input values are in bound in the image matrix
+        int row: vertical position of the cell to be processed
+        int col: horizontal position of the cell to be processed
+        '''
         rowsInBound = False
         colsInBound = False
         if row > 0 and row < len(self.img) - 1:
--- medianDeterministic.py
+++ medianDeterministic.py
@@ -1,123 +1,67 @@
+"""
+@class:  CSC382
+@author: Francesco Pecora
+@date:   12/13/2020
+"""
+
 import random
 
-'''
-def kthSmallest(self, arr, l, r, k):  
-        
-        # If k is smaller than number of  
-        # elements in array  
-        if (k > 0 and k <= r - l + 1):  
-            
-            # Number of elements in arr[l..r]  
-            n = r - l + 1
-    
-            # Divide arr[] in groups of size 5,  
-            # calculate median of every group 
-            # and store it in median[] array.  
-            median = [] 
-    
-            i = 0
-            while (i < n // 5): 
-                median.append(self.findMedian(arr, l + i * 5, 5)) 
-                i += 1
-    
-            # For last group with less than 5 elements  
-            if (i * 5 < n): 
-                median.append(self.findMedian(arr, l + i * 5,  
-                                                n % 5)) 
-                i += 1
-    
-            # Find median of all medians using recursive call.  
-            # If median[] has only one element, then no need  
-            # of recursive call 
-            if i == 1: 
-                medOfMed = median[i - 1] 
-            else: 
-                medOfMed = self.kthSmallest(median, 0,  
-                                    i - 1, i // 2) 
-    
-            # Partition the array around a medOfMed 
-            # element and get position of pivot  
-            # element in sorted array  
-            pos = self.partition(arr, l, r, medOfMed) 
-    
-            # If position is same as k  
-            if (pos - l == k - 1):  
-                return arr[pos]  
-            if (pos - l > k - 1): # If position is more,  
-                                # recur for left subarray  
-                return self.kthSmallest(arr, l, pos - 1, k)  
-    
-            # Else recur for right subarray  
-            return self.kthSmallest(arr, pos + 1, r, k - pos + l - 1)  
-    
-        # If k is more than the number of  
-        # elements in the array
-        return 9999999
-    
-    def swap(self, arr, a, b):  
-        temp = arr[a]  
-        arr[a] = arr[b]  
-        arr[b] = temp  
-    
-    # It searches for x in arr[l..r],   
-    # and partitions the array around x.  
-    def partition(self, arr, l, r, x): 
-        for i in range(l, r): 
-            if arr[i] == x: 
-                self.swap(arr, r, i) 
-                break
-    
-        x = arr[r]  
-        i = l  
-        for j in range(l, r):  
-            if (arr[j] <= x):  
-                self.swap(arr, i, j)  
-                i += 1
-        self.swap(arr, i, r)  
-        return i  
-    
-    # A simple function to find  
-    # median of arr[] from index l to l+n 
-    def findMedian(self, arr, l, n): 
-        lis = [] 
-        for i in range(l, l + n): 
-            lis.append(arr[i]) 
-            
-        # Sort the array  
-        lis.sort() 
-    
-        # Return the middle element 
-        return lis[n // 2]
-
-'''
-
 class DeterministicMedian:
-    def quickselect_median(self, l, pivot_fn=random.choice):
-        if len(l) % 2 == 1:
-            return self.quickselect(l, len(l) // 2, pivot_fn)
-        else:
-            return 0.5 * (self.quickselect(l, len(l) // 2 - 1, pivot_fn) +
-                        self.quickselect(l, len(l) // 2, pivot_fn))
 
+    def kthSmallest(self, arr, l, r, k): 
+        '''
+        main function that finds the kth smallest value
+        list arr: list holding our values
+        int l: integer holding left pointer (start of list)
+        int r: integer holding right pointer (end of list)
+        int k: integer holding kth position required
+        '''
+        if (k > 0 and k <= r - l + 1): # k less than num of elements in array
+            pos = self.randomPartition(arr, l, r) 
+            if (pos - l == k - 1): 
+                return arr[pos] 
+            if (pos - l > k - 1):
+                return self.kthSmallest(arr, l, pos - 1, k) 
+            return self.kthSmallest(arr, pos + 1, r, k - pos + l - 1) 
+        return 99999999
+    
 
-    def quickselect(self, l, k, pivot_fn):
-        
-        if len(l) == 1:
-            assert k == 0
-            return l[0]
+    def swap(self, arr, a, b):
+        '''
+        function that swaps two elements of an array
+        list arr: list of elements
+        int a: first index to be swapped
+        int b: second index to be swapped
+        '''
+        arr[a], arr[b] = arr[b], arr[a]
+    
 
-        pivot = pivot_fn(l)
+    def partition(self, arr, l, r):
+        '''
+        function that creates the partition of the array
+        list arr: list holding our values
+        int l: integer holding left pointer (start of partition)
+        int r: integer holding right pointer (end of partition)
+        '''
+        x = arr[r] 
+        i = l 
+        for j in range(l, r): 
+            if (arr[j] <= x): 
+                self.swap(arr, i, j) 
+                i += 1
+        self.swap(arr, i, r) 
+        return i
+ 
 
-        lows = [el for el in l if el < pivot]
-        highs = [el for el in l if el > pivot]
-        pivots = [el for el in l if el == pivot]
-
-        if k < len(lows):
-            return self.quickselect(lows, k, pivot_fn)
-        elif k < len(lows) + len(pivots):
-            # We got lucky and guessed the median
-            return pivots[0]
-        else:
-            return self.quickselect(highs, k - len(lows) - len(pivots), pivot_fn)
-        
+    def randomPartition(self, arr, l, r): 
+        '''
+        function finding random partition of array
+        list arr: list holding our values
+        int l: integer holding left pointer (start of list)
+        int r: integer holding right pointer (end of list)
+        '''
+        n = r - l + 1
+        pivot = int(random.random() % n) 
+        self.swap(arr, l + pivot, r) 
+        return self.partition(arr, l, r) 
         
